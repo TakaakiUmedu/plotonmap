3aeed38a376c113cf0030dcf5dbfbfe55da8cfad
Takaaki Umedu <umedu>
2013-01-17 00:52:39 +0900

    ver. 0.9.4, debug flag disabled

--- pack.bat
+++ pack.bat
@@ -1,2 +1,2 @@
 del plotonmap.xpi
-zip -r plotonmap.0.9.3.xpi chrome defaults locale install.rdf chrome.manifest LICENSE README
+zip -r plotonmap.0.9.4.xpi chrome defaults locale install.rdf chrome.manifest LICENSE README
